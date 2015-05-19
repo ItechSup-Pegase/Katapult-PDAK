09b39ef4f7e934e4cc7540e69ede2ec738c337a2
Padow <patrick.douchement@gmail.com>
2015-05-19 12:19:56 +0200

    trolololo

---
+++ src/Sofitech/AdminBundle/Form/CompanyType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sofitech\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CompanyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('adresses')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sofitech\AdminBundle\Entity\Company'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sofitech_adminbundle_company';
+    }
+}
