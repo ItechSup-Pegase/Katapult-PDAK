6aeb9816095ae4615d7608168186adf2e8707664
Padow <patrick.douchement@gmail.com>
2015-05-28 09:19:25 +0200

    add formation

---
+++ src/Sofitech/AdminBundle/Entity/Formation.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Sofitech\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Formation
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Sofitech\AdminBundle\Entity\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=1000)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="duration", type="integer")
+     */
+    private $duration;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="program", type="text")
+     */
+    private $program;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Category", inversedBy="formations",cascade={"persist"}) 
+     * @ORM\JoinTable(name="formations_categories")
+     *
+     */
+    private $categories;
+
+    public function __construct(){
+        $this->categories = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Fomation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Fomation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     * @return Fomation
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return integer 
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set program
+     *
+     * @param string $program
+     * @return Fomation
+     */
+    public function setProgram($program)
+    {
+        $this->program = $program;
+
+        return $this;
+    }
+
+    /**
+     * Get program
+     *
+     * @return string 
+     */
+    public function getProgram()
+    {
+        return $this->program;
+    }
+
+    /**
+     * Set categories
+     *
+     * @param \stdClass $categories
+     * @return Fomation
+     */
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return \stdClass 
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+}
---
+++ src/Sofitech/AdminBundle/Form/FormationType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sofitech\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FormationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('duration')
+            ->add('program')
+            ->add('categories')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sofitech\AdminBundle\Entity\Formation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sofitech_adminbundle_formation';
+    }
+}
